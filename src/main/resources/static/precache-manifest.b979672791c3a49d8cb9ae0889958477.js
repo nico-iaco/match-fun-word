a6189683095d20f0eb7997a4d55603c41a72dfef
Nicola Iacovelli <nicolaiacovelli98@gmail.com>
2020-04-18 23:08:09 +0200

    Added FE build

---
+++ src/main/resources/static/precache-manifest.b979672791c3a49d8cb9ae0889958477.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6de5881223c704d6e841dbfe43690c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0f44db233f644d7f07e",
+    "url": "/static/css/main.c0a58957.chunk.css"
+  },
+  {
+    "revision": "e29690b566aea71f73d6",
+    "url": "/static/js/2.293d3511.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.293d3511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0f44db233f644d7f07e",
+    "url": "/static/js/main.e7faa91e.chunk.js"
+  },
+  {
+    "revision": "ae42438e1c5ab191ada9",
+    "url": "/static/js/runtime-main.af72910b.js"
+  }
+]);
